48f4253e25ccf3ea5bf0e05432d166d1265e96db
unknown <mohamedzahertaha@gmail.com>
2023-10-02 23:25:21 +0300

    3th

---
+++ main.js
@@ -0,0 +1,24 @@
+daysP = document.querySelector("div.Days > p.num");
+hoursP = document.querySelector("div.Hours > p.num");
+minutesP = document.querySelector("div.Minutes > p.num");
+secondsP = document.querySelector("div.Seconds > p.num");
+console.log(daysP);
+console.log(hoursP);
+console.log(minutesP);
+console.log(secondsP);
+
+let dateTarget = new Date("13 July 2024 12:00:00");
+setInterval(() => {
+  let dateNow = new Date().getTime();
+  let dateDiff = dateTarget - dateNow;
+  let DaysT = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let HoursT = Math.floor(
+    (dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let MinutesT = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let SecondsT = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  daysP.innerHTML = DaysT;
+  hoursP.innerHTML = HoursT;
+  minutesP.innerHTML = MinutesT;
+  secondsP.innerHTML = SecondsT;
+}, 1000);
