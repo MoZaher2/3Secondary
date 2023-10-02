0f5f7238bfe5c004d72c0341a3fd8804d1733232
unknown <mohamedzahertaha@gmail.com>
2023-10-02 23:39:30 +0300

    Author name

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ daysP = document.querySelector("div.Days > p.num");
 hoursP = document.querySelector("div.Hours > p.num");
 minutesP = document.querySelector("div.Minutes > p.num");
 secondsP = document.querySelector("div.Seconds > p.num");
-console.log(daysP);
-console.log(hoursP);
-console.log(minutesP);
-console.log(secondsP);
 
 let dateTarget = new Date("13 July 2024 12:00:00");
 setInterval(() => {
